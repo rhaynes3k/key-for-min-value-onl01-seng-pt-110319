7f410c56a3e52d9a71550533574338e7aa7fe5fe
Rommel Haynes <rhaynes3k@gmail.com>
2019-11-20 00:56:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  unless name_hash.empty?
+    name_hash.values.sort[0]
+  else
+  
+  end
 end
