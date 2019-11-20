8a04cb8f0d48b409eea30e53c54f404b1b4acfc3
Rommel Haynes <rhaynes3k@gmail.com>
2019-11-20 03:52:01 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  unless name_hash.empty?
+  new_a = []
+    new_a << name_hash.map.sort_by do|k, v|v
+    end
+    new_a.flatten
+    new_a[0][0][0]
+  end
 end
